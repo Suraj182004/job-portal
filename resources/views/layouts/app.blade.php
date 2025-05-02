715a6d11160dbb0e12f08ac93393c74fff8d0683
Suraj182004 <surajyaligar2004@gmail.com>
2025-05-02 23:22:15 +0530

    done

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>{{ config('app.name', 'JobSphere') }}</title>
-
+<!--Title-->
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600,700,800&display=swap" rel="stylesheet" />
